9d5dc6780ac592c669f0b5132a89384ab1f46787
c1rabbit <mail@calvinthanh.com>
2015-12-08 20:48:59 -0800

    resolved disconnect for RR

--- net/response/ResponseDisconnect.py
+++ net/response/ResponseDisconnect.py
@@ -9,12 +9,19 @@ class ResponseDisconnect(ServerResponse):
 
         try:
             username = data.getString()
-            if username in self.world.vehiclelist.keys():
-                #self.world.vehiclelist[username].chassisNP.remove()
-                self.world.vehiclelist[username].remove(self.world)
-                #self.world.world.removeVehicle(self.world.vehiclelist[username].vehicle)
-                #self.world.vehiclelist[username].chassisNode.remove()
-                self.world.vehiclelist.pop(username)
+
+            if self.worldMgr.isDDGame:
+                if username in self.world.vehiclelist.keys():
+                    #self.world.vehiclelist[username].chassisNP.remove()
+                    self.world.vehiclelist[username].remove(self.world)
+                    #self.world.world.removeVehicle(self.world.vehiclelist[username].vehicle)
+                    #self.world.vehiclelist[username].chassisNode.remove()
+                    self.world.vehiclelist.pop(username)
+            else:
+                if username in self.world.vehiclelist.keys():
+                    self.world.vehiclelist[username].remove()
+                    self.world.vehiclelist.pop(username)
+
 
         except:
             self.log('Bad [' + str(Constants.SMSG_DISCONNECT) + '] Disconnect Response')
