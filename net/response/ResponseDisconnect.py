c5fc869750e3e52a6675b85e1e916fabda395364
genusgant <genusgant@gmail.com>
2015-12-08 21:03:01 -0800

    handling disconnect

--- net/response/ResponseDisconnect.py
+++ net/response/ResponseDisconnect.py
@@ -9,12 +9,31 @@ class ResponseDisconnect(ServerResponse):
 
         try:
             username = data.getString()
-            #self.world.vehiclelist[username].chassisNP.remove()
-            self.world.vehiclelist[username].remove(self.world)
-            #self.world.world.removeVehicle(self.world.vehiclelist[username].vehicle)
-            #self.world.vehiclelist[username].chassisNode.remove()
-            self.world.vehiclelist.pop(username)
 
+            print "isDDGame Disconnect Response"
+
+            if self.worldMgr.isDDGame:
+                print "isDDGame"
+                if username in self.world.vehiclelist.keys():
+                    vehicle = self.world.vehiclelist[username]
+                    vehicle.props.health = vehicle.props.armor = 0
+                    vehicle.isDead = True
+                    vehicle.remove(self.world)
+                    # self.world.vehiclelist.pop(username)
+                    self.world.deadCounter +=1
+                    #del self.world.vehiclelist[self.username]
+                    #self.world.vehiclelist[username].chassisNP.remove()
+                    # self.world.vehiclelist[username].remove(self.world)
+                    #self.world.world.removeVehicle(self.world.vehiclelist[username].vehicle)
+                    #self.world.vehiclelist[username].chassisNode.remove()
+                    # self.world.vehiclelist.pop(username)
+                    print "deadCounter :",self.world.deadCounter
+                    print "vehicle :",len(self.world.vehiclelist)
+                    if self.world.deadCounter == len(self.world.vehiclelist)-1:
+                        print "Last Man Standing"
+                        self.world.gameEnd()
+            # else:  # for RR game
+                        
         except:
             self.log('Bad [' + str(Constants.SMSG_DISCONNECT) + '] Disconnect Response')
             print_exc()
