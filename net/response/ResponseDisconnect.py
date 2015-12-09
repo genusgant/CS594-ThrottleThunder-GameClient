2cb7344155f8e21fed95d776b3bdd889642ff755
David Thomas <david.thomas214@yahoo.com>
2015-12-08 18:19:47 -0800

    adding a small patch

--- net/response/ResponseDisconnect.py
+++ net/response/ResponseDisconnect.py
@@ -9,11 +9,12 @@ class ResponseDisconnect(ServerResponse):
 
         try:
             username = data.getString()
-            #self.world.vehiclelist[username].chassisNP.remove()
-            self.world.vehiclelist[username].remove(self.world)
-            #self.world.world.removeVehicle(self.world.vehiclelist[username].vehicle)
-            #self.world.vehiclelist[username].chassisNode.remove()
-            self.world.vehiclelist.pop(username)
+            if username in self.world.vehiclelist.keys():
+                #self.world.vehiclelist[username].chassisNP.remove()
+                self.world.vehiclelist[username].remove(self.world)
+                #self.world.world.removeVehicle(self.world.vehiclelist[username].vehicle)
+                #self.world.vehiclelist[username].chassisNode.remove()
+                self.world.vehiclelist.pop(username)
 
         except:
             self.log('Bad [' + str(Constants.SMSG_DISCONNECT) + '] Disconnect Response')
