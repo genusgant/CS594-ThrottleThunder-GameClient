1403081daad192f5b37c44817b9d295224dcfa05
c1rabbit <mail@calvinthanh.com>
2015-12-08 16:03:32 -0800

    RR part blocked off

--- net/response/ResponseDead.py
+++ net/response/ResponseDead.py
@@ -1,40 +1,47 @@
 from traceback import print_exc
-
 from common.Constants import Constants
 from net.response.ServerResponse import ServerResponse
 
-class ResponseDead(ServerResponse):
 
+class ResponseDead(ServerResponse):
     def execute(self, data):
 
         try:
             self.username = data.getString()
-            # print "server sent:", self.username
-            # print self.world.vehiclelist.keys()
-            print "I am:", self.world.login
-            if self.world.login == self.username:
-                print "you are dead"
+            print "server sent dead guy:", self.username
 
-            if self.username in self.world.vehiclelist.keys():
-                vehicle = self.world.vehiclelist[self.username]
-                if self.worldMgr.isDDGame:
+            if self.worldMgr.isDDGame:
+                if self.username in self.world.vehiclelist.keys():
+                    vehicle = self.world.vehiclelist[self.username]
                     vehicle.props.health = vehicle.props.armor = 0
-                    #Handle removing character when dead
+                    vehicle.isDead = True
                     vehicle.remove()
-                else:  # for RR game
-                    vehicle.remove()
-                del self.world.vehiclelist[self.username]
-                self.world.deadCounter += 1
-
-                print "deadCounter/vehiclelist :", self.world.deadCounter, "/", len(self.world.vehiclelist)
-
-                if self.world.deadCounter == len(self.world.vehiclelist)-1:
-                    print "Last Man Standing"
+                    self.world.deadCounter += 1
+                    # del self.world.vehiclelist[self.username]
+                    if self.world.deadCounter == len(self.world.vehiclelist) - 1:
+                        print "Last Man Standing"
                     self.world.gameEnd()
+            else:  # for RR game
+                print "I am:", self.world.login
+                if self.world.login == self.username:
+                    print "you are dead"
+                    self.world.vehicleContainer.chassisNP.remove()
 
-                # vehicle.chassisNP.removeNode()
+                if self.username in self.world.vehiclelist.keys():
+                    vehicle = self.world.vehiclelist[self.username]
+                    vehicle.remove()
+                    del self.world.vehiclelist[self.username]
+                    self.world.deadCounter += 1
 
-            print "ResponseDead - ",self.username
+                    print "deadCounter/vehiclelist :", self.world.deadCounter, "/", len(self.world.vehiclelist)
+
+                    if self.world.deadCounter == len(self.world.vehiclelist) - 1:
+                        print "Last Man Standing"
+                        self.world.gameEnd()
+
+                        # vehicle.chassisNP.removeNode()
+
+            print "ResponseDead - ", self.username
             # self.log('Received [' + str(Constants.RAND_STRING) + '] String Response')
 
         except:
